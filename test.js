e275a8f1df6a36cdebf800fd33d4a704ab6d7a87
karthiksriram1443-cloud <karthiksriram1443@gmail.com>
2026-01-30 18:09:01 +0530

    basic javascript

--- test.js
+++ test.js
@@ -1,1 +1,44 @@
-console.log("Hi")
+let x = 5;
+let y = 10;
+let z = x + y;
+console.log(z);
+
+const a = 15;
+const b = 10;
+const c = a - b;
+console.log(c);
+
+let _lastName = "smith";
+let _x = 2;
+let _100 = 5;
+console.log(_lastName+" "+_x+" "+_100);
+
+let _Name = "john " + "deo";
+console.log(_Name);
+
+let _firstName = "johanson";
+let _g = 2;
+let _101 = 6;
+console.log(_firstName+" "+_g+" "+_101);
+
+let $ = "hello world";
+let $$$ = 4;
+let $mymoney = 7;
+console.log($+" "+$$$+" "+$mymoney);
+
+let e=true;
+let f=false;
+console.log(e != f);
+
+let text = `he 's often called "johnny"`;
+console.log(text);
+
+let n =50
+let r =[]
+ for (let i=1;i<=n;i++)
+ {
+    if(i%3==0){
+    console.log(i)
+ }
+ }
+ 
